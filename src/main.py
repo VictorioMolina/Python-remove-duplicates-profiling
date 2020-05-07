dd947f539a5ed0308d98586035a4826144fccc3c
Victorio Molina <victoriojesus.molinab@um.es>
2020-05-07 02:22:14 +0200

    Preparation of the main file. Control of arguments received by command line.

---
+++ src/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Created on Thu May  7 01:40:07 2020
+
+@author: VictorioMolina
+'''
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():   
+    # Command line argument control
+    if len(sys.argv) != 4:
+        print("Usage: {} infile outfile pdf_outfile".format(sys.argv[0]))
+        sys.exit(0)
+
+if __name__ == '__main__':
+    main()
